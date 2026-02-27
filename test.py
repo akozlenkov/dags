86cdd249787fd7668099f1493649750bf5aa90d8
Andrey Kozlenkov <andrey.kozlenkov@gmail.com>
2026-02-27 20:29:09 +0300

    first commit

---
+++ test.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.providers.standard.operators.python import PythonOperator
+
+# Функция, которую будем запускать
+def hello_world(**kwargs):
+    print("Hello, Airflow 3.1.7!")
+    return "Done"
+
+# Определяем DAG
+with DAG(
+    dag_id="python_operator_example",
+    start_date=datetime(2026, 2, 27),
+    schedule_interval="@daily",
+    catchup=False,
+    tags=["example"],
+) as dag:
+
+    task_hello = PythonOperator(
+        task_id="say_hello",
+        python_callable=hello_world,
+        provide_context=True,  # для доступа к kwargs
+    )
+
+    task_hello
+
