8324bd2470c2a75232e21359f0163f7a068501cf
Andrey Kozlenkov <andrey.kozlenkov@gmail.com>
2026-02-27 21:46:55 +0300

    first commit

--- dags/test.py
+++ dags/test.py
@@ -26,55 +26,55 @@ with DAG(
                     image="apache/airflow:3.1.7-python3.12",
                     command=["bash", "-c"],
                     args=["exec airflow worker"],  # можно заменить на любой запуск
-                    env=[
-                        k8s.V1EnvVar(
-                            name="AIRFLOW__DATABASE__SQL_ALCHEMY_CONN",
-                            value_from=k8s.V1EnvVarSource(
-                                secret_key_ref=k8s.V1SecretKeySelector(
-                                    name="my-airflow-database",
-                                    key="connection"
-                                )
-                            )
-                        ),
-                        k8s.V1EnvVar(
-                            name="AIRFLOW__CORE__FERNET_KEY",
-                            value_from=k8s.V1EnvVarSource(
-                                secret_key_ref=k8s.V1SecretKeySelector(
-                                    name="my-airflow-fernet-key",
-                                    key="fernet-key"
-                                )
-                            )
-                        ),
-                        k8s.V1EnvVar(
-                            name="AIRFLOW__API_AUTH__JWT_SECRET",
-                            value_from=k8s.V1EnvVarSource(
-                                secret_key_ref=k8s.V1SecretKeySelector(
-                                    name="my-airflow-jwt-secret",
-                                    key="jwt-secret"
-                                )
-                            )
-                        ),
-                    ],
-                    volume_mounts=[
-                        k8s.V1VolumeMount(
-                            name="config",
-                            mount_path="/opt/airflow/airflow.cfg",
-                            sub_path="airflow.cfg",
-                            read_only=True,
-                        )
-                    ],
+ #                   env=[
+ #                       k8s.V1EnvVar(
+ #                           name="AIRFLOW__DATABASE__SQL_ALCHEMY_CONN",
+ #                           value_from=k8s.V1EnvVarSource(
+ #                               secret_key_ref=k8s.V1SecretKeySelector(
+ #                                   name="my-airflow-database",
+ #                                   key="connection"
+ #                               )
+ #                           )
+ #                       ),
+ #                       k8s.V1EnvVar(
+ #                           name="AIRFLOW__CORE__FERNET_KEY",
+ #                           value_from=k8s.V1EnvVarSource(
+ #                               secret_key_ref=k8s.V1SecretKeySelector(
+ #                                   name="my-airflow-fernet-key",
+ #                                   key="fernet-key"
+ #                               )
+ #                           )
+ #                       ),
+ #                       k8s.V1EnvVar(
+ #                           name="AIRFLOW__API_AUTH__JWT_SECRET",
+ #                           value_from=k8s.V1EnvVarSource(
+ #                               secret_key_ref=k8s.V1SecretKeySelector(
+ #                                   name="my-airflow-jwt-secret",
+ #                                   key="jwt-secret"
+ #                               )
+ #                           )
+ #                       ),
+ #                   ],
+ #                   volume_mounts=[
+ #                       k8s.V1VolumeMount(
+ #                           name="config",
+ #                           mount_path="/opt/airflow/airflow.cfg",
+ #                           sub_path="airflow.cfg",
+ #                           read_only=True,
+ #                       )
+ #                   ],
                 )
             ],
-            volumes=[
-                k8s.V1Volume(
-                    name="config",
-                    config_map=k8s.V1ConfigMapVolumeSource(
-                        name="my-airflow-config",
-                        default_mode=0o644
-                    )
-                )
-            ],
-            service_account_name="airflow-serviceaccount",  # при необходимости
+ #           volumes=[
+ #               k8s.V1Volume(
+ #                   name="config",
+ #                   config_map=k8s.V1ConfigMapVolumeSource(
+ #                       name="my-airflow-config",
+ #                       default_mode=0o644
+ #                   )
+ #               )
+ #           ],
+            service_account_name="airflow",  # при необходимости
         ),
         metadata=k8s.V1ObjectMeta(labels={"release": "stable"}),
     )
