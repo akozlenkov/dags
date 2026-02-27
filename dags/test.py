31bd8b52b7dee9905a6a1e9e3ab4c76a91744afb
Andrey Kozlenkov <andrey.kozlenkov@gmail.com>
2026-02-27 20:39:01 +0300

    first commit

--- dags/test.py
+++ dags/test.py
@@ -1,26 +1,33 @@
 from datetime import datetime
 from airflow import DAG
 from airflow.providers.standard.operators.python import PythonOperator
+from airflow.operators.empty import EmptyOperator
 
-# Функция, которую будем запускать
 def hello_world(**kwargs):
-    print("Hello, Airflow 3.1.7!")
-    return "Done"
+    print("Hello from KubernetesExecutor!")
 
-# Определяем DAG
 with DAG(
-    dag_id="python_operator_example",
+    dag_id="python_operator_k8s111",
     start_date=datetime(2026, 2, 27),
-    schedule_interval="@daily",
+    schedule="@daily",
     catchup=False,
     tags=["example"],
 ) as dag:
 
+    start = EmptyOperator(task_id="start")
+
     task_hello = PythonOperator(
         task_id="say_hello",
         python_callable=hello_world,
-        provide_context=True,  # для доступа к kwargs
+        executor_config={
+            "KubernetesExecutor": {
+                "image": "apache/airflow:3.1.7-python3.12"
+            }
+        },
     )
 
-    task_hello
+    end = EmptyOperator(task_id="end")
+
+    start >> task_hello >> end
+
 
